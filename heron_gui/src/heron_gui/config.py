1f5ae1f110920006f9d6b11775a2fcc77d2349e2
Dongwook Lee <dongwooklee1201@Dongwooks-MacBook-Air.local>
2022-10-06 12:33:48 +0900

    structured code

--- heron_gui/src/heron_gui/config.py
+++ heron_gui/src/heron_gui/config.py
@@ -3,28 +3,23 @@
 import os
 root_dir = os.path.dirname(__file__)
 
-# Reference Map Image Path
-# 오리연못 가로 126m, 세로 179m (126/179 = 0.7039)
-# 연못사진 가로 523px, 세로 743px (523/743 = 0.7039)
-# Plot 크기 가로 7in, 세로 9.944in (9.944/7 =0.7039)
+MAP_PATH = os.path.join(root_dir, "../images/map_satel.png")    # map_common.png | map_contour.png | map_satel.png
 
-# MAP_PATH = os.path.join(root_dir, "../images/map_common.png")
-MAP_PATH = os.path.join(root_dir, "../images/map_satel.png")
-# MAP_PATH = os.path.join(root_dir, "../images/map_contour.png")
-
-MAP_ORIGIN_LLA = [36.3672449, 127.3619089, 0.0]
-MAP_ORIGIN_UTM = [353044.30, 4025928.39, 0.0]
-MAP_WIDTH = 126
-MAP_HEIGHT = 181
+MAP_ORIGIN_LLA = [36.3672449, 127.3619089, 0.0]     # LLA value of (0, 0) position on the map image
+MAP_ORIGIN_UTM = [353044.30, 4025928.39, 0.0]       # LLA value transformed to UTM via https://www.latlong.net/lat-long-utm.html
+MAP_WIDTH = 126     # 지도 실제 너비 (m)
+MAP_HEIGHT = 181    # 지도 실제 높이 (m)
+_PRECISION = 7      # lla, utm decimal precision
 
 # Waypoint CSV File Path
-CSV_PATH = os.path.join(root_dir, "../csv_files/square.csv")
-EXPORT_CSV_PATH = os.path.join(root_dir, "../csv_files/export_test.csv")
-MAX_WP2PLOT = 5 # Change Line 83 of main_gui.py as well (add more buttons)
+CSV_PATH = os.path.join(root_dir, "../csv_files/square.csv")                # CSV file to read in '-m wp_read' mode
+EXPORT_CSV_PATH = os.path.join(root_dir, "../csv_files/export_test.csv")    # CSV file to export from '-m wp_plot' mode
+MAX_WP2PLOT = 5                                                             # number of waypoints to export from '-m wp_plot' mode
+                                                                            # Change Line 83 of main_gui.py as well (add more Buttons)
 
 # Subscriber Name
-GPS_SUBSCRIBER = "/ublox_gps/fix"
-UTM_SUBSCRIBER = "/Heron_UTMPose"
+GPS_SUBSCRIBER = "/ublox_gps/fix"   # ROSTOPIC sent from GPS sensor
+UTM_SUBSCRIBER = "/Heron_UTMPose"   # ROSTOPIC sent from lla2utm roslaunch
 
 # COLOR PALETTE
 WAYPOINT_COLOR = "black"
